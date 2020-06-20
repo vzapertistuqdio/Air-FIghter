ac110ff12146a3246c2387b5c643debabe1d335b
vzapertistudio <vzapertistudio@gmail.com>
2020-06-20 09:19:42 +0300

    Revert "Code refactoring and delete useless scripts"
    This reverts commit 31c360e1212514e1bbafb6b325ba18f7ba879c26.

---
+++ FLAPPY/Assets/Scripts/MainMenu/InventoryInitialize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryInitialize : MonoBehaviour
+{
+    private Inventory inventory;
+    private void Start()
+    {
+        inventory = Inventory.GetInstance();
+        inventory.Add(101);
+        inventory.Add(201);
+        inventory.Save();
+        if (PlayerPrefs.GetInt("CurrentPlane")==0)
+            PlayerPrefs.SetInt("CurrentPlane", 101);
+        if (PlayerPrefs.GetInt("CurrentPilot") == 0)
+            PlayerPrefs.SetInt("CurrentPilot", 201);
+        
+
+    }
+   
+
+}
---
+++ FLAPPY/Assets/Scripts/PlayScene/MouseOnJoystick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+
+public class MouseOnJoystick : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+{
+    public bool isOnCircle = false;
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isOnCircle = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isOnCircle = false;
+    }
+
+   
+}
